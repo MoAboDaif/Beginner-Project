5b2b185766c3e42544cf0265a6e2b185419c2e9f
Mohamed AboDaif <Mohamed.AboDaif@ppc.com.eg>
2025-08-04 10:11:50 +0300

    fix: update workflow and terraform files
    modified:   .github/workflows/deploy.yml
    modified:   terraform/output.tf
    deleted:    terraform/policies/s3_static_website.json

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,6 @@
 output "static_website_url" {
-  value = "http://${aws_s3_bucket.static_website.bucket_regional_domain_name}"
+  value = "http://${aws_s3_bucket.static_website.website_endpoint}"
+}
+output "s3_bucket_name" {
+  value = aws_s3_bucket.static_website.bucket
 }
